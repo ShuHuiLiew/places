a4fe590e35fb60b9d4dc747981b6fc60a1185fbd
shu hui <shuhuiliew@gmial.com>
2018-07-10 12:57:21 +0800

    Added form, historical, main

---
+++ client/src/components/Historical.js
@@ -0,0 +1,15 @@
+//Navbar is a class; All class is needed to start with Capital letter
+import React, { Component } from 'react';
+
+class Historical extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Historical works</h1>
+      </div>
+
+    );
+  }
+}
+
+export default Historical;
---
+++ client/src/components/Main.js
@@ -0,0 +1,15 @@
+//Navbar is a class; All class is needed to start with Capital letter
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Main works</h1>
+      </div>
+
+    );
+  }
+}
+
+export default Main;
